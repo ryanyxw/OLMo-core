692f344ee4b5c1dee06ffafc606c839a363ae51f
epwalsh <petew@allenai.org>
2025-03-13 14:34:24 -0700

    prep basic matmul

---
+++ src/olmo_core/ops/utils.py
@@ -0,0 +1,10 @@
+import triton  # type: ignore
+
+
+def is_cuda():
+    return triton.runtime.driver.active.get_current_target().backend == "cuda"
+
+
+def is_hip_mi200():
+    target = triton.runtime.driver.active.get_current_target()
+    return target.backend == "hip" and target.arch == "gfx90a"

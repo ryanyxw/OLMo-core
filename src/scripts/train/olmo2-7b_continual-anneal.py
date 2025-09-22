8afd09001364ce8a9efeb36aec1b5e0c74e869b9
ryanyxw <ryanywan@usc.edu>
2025-09-22 02:13:10 -0700

    update

--- src/scripts/train/olmo2-7b_continual-anneal.py
+++ src/scripts/train/olmo2-7b_continual-anneal.py
@@ -218,7 +218,7 @@ def build_config(opts, overrides: List[str]) -> ExperimentConfig:
             save_overwrite=True,
             metrics_collect_interval=5,
             cancel_check_interval=5,
-            load_path="/weka/oe-training-default/ai2-llm/checkpoints/OLMo-medium/peteish7/step928646/train",
+            load_path="/weka/oe-training-default/ryanwang/phdbrainstorm/OLMo-core/olmo2_7b_converted/model_and_optim",
             load_strategy=LoadStrategy.always,
         )
         .with_callback("gpu_monitor", GPUMemoryMonitorCallback())
